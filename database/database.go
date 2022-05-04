0a7e545435b44a14c93f6054f294674a17e00738
Mariano Fuhr <mfuhr91@gmail.com>
2022-05-04 00:43:51 -0300

    update Procfile for heroku

--- database/database.go
+++ database/database.go
@@ -2,13 +2,24 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 )
 
 func DatabaseConnect() (*sql.DB, error) {
 	var db *sql.DB
 	var err error
-	db, err = sql.Open("mysql", "admin:2744mfuhR@/starbucks_db")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	host := os.Getenv("DB_HOST")
+	if dbName == "" {
+		dbName = "starbucks_db"
+	}
+	dataSource := fmt.Sprintf("%s:%s@%s/%s", user, password, host, dbName)
+	
+	db, err = sql.Open("mysql", dataSource)
 	if err != nil {
 		log.Fatalf("error when trying to connect to starbucks_db, %s", err.Error())
 		return db, nil
