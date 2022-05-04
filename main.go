f65939422743a44ff1d2c0c4b9cadd0805374d8f
Mariano Fuhr <mfuhr91@gmail.com>
2022-05-04 01:03:39 -0300

    add getting port from envs

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"os"
 	"starbucks-app/routes"
 )
 
@@ -11,12 +13,14 @@ func main() {
 	
 	routes.InitRoutes(router)
 	
-	/*port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	
 	if port == "" {
-		port = ":8080"
-	}*/
-	err := router.Run(":8080")
+		port = "4747"
+	}
+	log.Printf("listen and serving on port: %s", port)
+	address := fmt.Sprintf(":%s", port)
+	err := router.Run(address)
 	if err != nil {
 		log.Fatalf("Cannot start the server: %v ", err.Error())
 		return
